86ae3e97c72252156704c10502340cbe508e0da6
MariamZead <mariam.a.zead@gmail.com>
2024-06-02 20:30:23 +0300

    🐛 Fix bug

--- src/js/model.js
+++ src/js/model.js
@@ -43,6 +43,8 @@ export const loadSearchResults = async function (query) {
         image: recipe.image_url,
       };
     });
+    // whenever we call loadSearchResults we want to reset the page to 1
+    state.search.page = 1;
   } catch (err) {
     console.error(`${err} 🔥🔥🔥`);
     throw err;
