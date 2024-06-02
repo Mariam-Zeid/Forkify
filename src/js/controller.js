c3450952312074967c253673bd033f044109c889
MariamZead <mariam.a.zead@gmail.com>
2024-06-02 13:43:27 +0300

    Initialize helper and config setup along with other files

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,19 +1,10 @@
 import * as model from "./model";
 import recipeView from "./views/recipeView";
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 ///////////////////////////////////////
 const controlRecipe = async function () {
   try {
     const id = window.location.hash.slice(1); // #527f7f7 => 527f7f7
-    console.log(id);
     // guard clause
     if (!id) return;
     recipeView.renderSpinner();
