e0a8299093b537e080d58a0eef75d43ea51ddec6
MariamZead <mariam.a.zead@gmail.com>
2024-06-02 13:34:13 +0300

    Fetch and render recipe details

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,5 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from "./model";
+import recipeView from "./views/recipeView";
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -8,6 +9,25 @@ const timeout = function (s) {
   });
 };
 
-// https://forkify-api.herokuapp.com/v2
-
 ///////////////////////////////////////
+const controlRecipe = async function () {
+  try {
+    const id = window.location.hash.slice(1); // #527f7f7 => 527f7f7
+    console.log(id);
+    // guard clause
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1) Loading recipe
+    await model.loadRecipe(id);
+
+    // 2) Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+["hashchange", "load"].forEach((event) =>
+  window.addEventListener(event, controlRecipe)
+);
